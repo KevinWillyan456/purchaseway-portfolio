ce062e079d5ed369d66d2b8f4ad06e7ba3a4600e
Kevin Souza <kevinwillyansouza@gmail.com>
2023-08-25 19:36:47 -0400

    feat: adicionando responsividade para telas mobile

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import "./Header.css";
 
 import logo from "/purchaseway-logo.png";
 
 function Header() {
+  const [menu, setMenu] = useState(false);
+
+  const handleMenu = () => {
+    setMenu(!menu);
+  };
+
   return (
     <header className="cabecalho">
       <nav className="nav">
@@ -23,6 +30,25 @@ function Header() {
             <a href="#">Contato</a>
           </li>
         </ul>
+        <ul className={menu ? "nav-mobile" : "nav-mobile hidden"}>
+          <li>
+            <a href="#">Início</a>
+          </li>
+          <li>
+            <a href="#">Sobre</a>
+          </li>
+          <li>
+            <a href="#">Serviços</a>
+          </li>
+          <li>
+            <a href="#">Contato</a>
+          </li>
+        </ul>
+        <div className="hamburger" onClick={handleMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </nav>
     </header>
   );
