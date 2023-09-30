579ae50423e28aee0a4b537a3f10c569410f5c07
Kevin Souza <kevinwillyansouza@gmail.com>
2023-09-30 15:01:20 -0400

    feat: criação do roteamento com react-router-dom

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import Banner from "../components/banner/Banner";
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+import Main from "../components/main/Main";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
