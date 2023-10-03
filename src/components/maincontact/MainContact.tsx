149a839bc7dcff5529500726b7bc7a30d440779a
Kevin Souza <kevinwillyansouza@gmail.com>
2023-10-03 13:37:59 -0400

    feat: melhoria nos estilos e validação no formulário

--- src/components/maincontact/MainContact.tsx
+++ src/components/maincontact/MainContact.tsx
@@ -1,6 +1,56 @@
+import { FormEvent, useState } from "react";
+import validator from "validator";
 import "./MainContact.css";
 
 function MainContact() {
+    const [nameInput, setNameInput] = useState("");
+    const [lastNameInput, setLastNameInput] = useState("");
+    const [emailInput, setEmailInput] = useState("");
+    const [messageInput, setMessageInput] = useState("");
+
+    const [nameInputEmpty, setNameInputEmpty] = useState(false);
+    const [lastNameInputEmpty, setLastNameInputEmpty] = useState(false);
+    const [emailInputEmpty, setEmailInputEmpty] = useState(false);
+    const [messageInputEmpty, setMessageInputEmpty] = useState(false);
+
+    const handleFormSubmit = (event: FormEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+
+        if (nameInput === "") {
+            setNameInputEmpty(true);
+        }
+        if (nameInput) {
+            setNameInputEmpty(false);
+        }
+        if (lastNameInput === "") {
+            setLastNameInputEmpty(true);
+        }
+        if (lastNameInput) {
+            setLastNameInputEmpty(false);
+        }
+        if (validator.isEmail(emailInput)) {
+            setEmailInputEmpty(false);
+        } else {
+            setEmailInputEmpty(true);
+        }
+        if (messageInput === "") {
+            setMessageInputEmpty(true);
+        }
+        if (messageInput) {
+            setMessageInputEmpty(false);
+        }
+        if (
+            !nameInput ||
+            !lastNameInput ||
+            !validator.isEmail(emailInput) ||
+            !messageInput
+        ) {
+            return;
+        }
+
+        alert("Enviado com suceeso!");
+    };
+
     return (
         <main className="main-contact">
             <section className="main-content">
@@ -19,18 +69,71 @@ function MainContact() {
                         </div>
                     </div>
                     <div className="main-form">
-                        <form className="main-form-tag">
+                        <form
+                            className="main-form-tag"
+                            onSubmit={handleFormSubmit}
+                        >
                             <div className="title">Formulário de Contato</div>
-                            <input type="text" placeholder="Digite seu nome" />
-                            <input
-                                type="text"
-                                placeholder="Digite seu sobrenome"
-                            />
-                            <input
-                                type="text"
-                                placeholder="Insira um endereço de e-mail válidob"
-                            />
-                            <textarea placeholder="Digite sua mensagem"></textarea>
+                            <div className="field">
+                                {nameInputEmpty && (
+                                    <div className="validate-message">
+                                        Preencha o campo nome
+                                    </div>
+                                )}
+                                <input
+                                    type="text"
+                                    placeholder="Digite seu nome"
+                                    value={nameInput}
+                                    onChange={(e) => {
+                                        setNameInput(e.target.value);
+                                    }}
+                                />
+                            </div>
+                            <div className="field">
+                                {lastNameInputEmpty && (
+                                    <div className="validate-message">
+                                        Preencha o campo sobrenome
+                                    </div>
+                                )}
+                                <input
+                                    type="text"
+                                    placeholder="Digite seu sobrenome"
+                                    value={lastNameInput}
+                                    onChange={(e) => {
+                                        setLastNameInput(e.target.value);
+                                    }}
+                                />
+                            </div>
+                            <div className="field">
+                                {emailInputEmpty && (
+                                    <div className="validate-message">
+                                        Este e-mail é inválido
+                                    </div>
+                                )}
+                                <input
+                                    type="text"
+                                    placeholder="Insira um endereço de e-mail válido"
+                                    value={emailInput}
+                                    onChange={(e) => {
+                                        setEmailInput(e.target.value);
+                                    }}
+                                />
+                            </div>
+                            <div className="field">
+                                {messageInputEmpty && (
+                                    <div className="validate-message">
+                                        Preencha o campo mensagem
+                                    </div>
+                                )}
+                                <textarea
+                                    placeholder="Digite sua mensagem"
+                                    maxLength={2000}
+                                    value={messageInput}
+                                    onChange={(e) => {
+                                        setMessageInput(e.target.value);
+                                    }}
+                                ></textarea>
+                            </div>
                             <button type="submit">Enviar</button>
                         </form>
                     </div>
